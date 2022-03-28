87c8457af750fd52e560af260192d805d46f951f
Zachary Gichiri <gichirigichuru@gmail.com>
2022-03-28 11:45:13 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { AuthGuard } from "./shared/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+  { path: 'log-in', component: SigninComponent },
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'user-profile/:id', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate:[AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
